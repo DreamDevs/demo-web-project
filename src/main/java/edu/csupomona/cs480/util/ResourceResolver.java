030b351540bcfb8a078335f14401f652dcc0dc0d
ksuzuki <kentarosuzuki.cs@gmail.com>
2016-10-23 18:44:14 -0700

    fixed  medList.json to be created in correct loction

--- src/main/java/edu/csupomona/cs480/util/ResourceResolver.java
+++ src/main/java/edu/csupomona/cs480/util/ResourceResolver.java
@@ -1,6 +1,8 @@
 package edu.csupomona.cs480.util;
 
 import java.io.File;
+import java.net.URL;
+
 
 /**
  * This is an utility class to help file locating.
@@ -9,6 +11,8 @@ public class ResourceResolver {
 
 	/** The base folder to store all the data used by this project. */
 	private static final String BASE_DIR = System.getProperty("user.home") + "/cs480";
+	static File file = new File("");
+	private static final String STATIC_DIR = file.getAbsolutePath() + "/src/main/resources/static";
 
 	/**
 	 * Get the file used to store the user object JSON
@@ -31,11 +35,10 @@ public class ResourceResolver {
 	 * @return
 	 */
 	public static File getMedFile() {
-		File file = new File(BASE_DIR + "/" + "medList.json");
+		File file = new File(STATIC_DIR + "/" + "medList.json");
 		if (!file.getParentFile().exists()) {
 			file.getParentFile().mkdirs();
 		}
 		return file;
 	}
-
 }
