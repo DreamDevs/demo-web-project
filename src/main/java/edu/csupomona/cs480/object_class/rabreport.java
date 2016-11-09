b2972ef11666f808ff2ef6ae2d5360eb8fac2ddb
ksuzuki <kentarosuzuki.cs@gmail.com>
2016-11-08 21:39:13 -0800

    tinker with these

---
+++ src/main/java/edu/csupomona/cs480/object_class/rabreport.java
@@ -0,0 +1,49 @@
+package edu.csupomona.cs480.object_class;
+
+public class rabreport {
+	double HGBA1C;
+	double LDL;
+	boolean Microalbumin;
+	String PTH;
+	String Calcium;
+	double GFR;
+	
+	public double getHGBA1C() {
+		return HGBA1C;
+	}
+	public void setHGBA1C(double hGBA1C) {
+		HGBA1C = hGBA1C;
+	}
+	public double getLDL() {
+		return LDL;
+	}
+	public void setLDL(double lDL) {
+		LDL = lDL;
+	}
+	public boolean isMicroalbumin() {
+		return Microalbumin;
+	}
+	public void setMicroalbumin(boolean microalbumin) {
+		Microalbumin = microalbumin;
+	}
+	public String getPTH() {
+		return PTH;
+	}
+	public void setPTH(String pTH) {
+		PTH = pTH;
+	}
+	public String getCalcium() {
+		return Calcium;
+	}
+	public void setCalcium(String calcium) {
+		Calcium = calcium;
+	}
+	public double getGFR() {
+		return GFR;
+	}
+	public void setGFR(double gFR) {
+		GFR = gFR;
+	}
+	
+
+}
